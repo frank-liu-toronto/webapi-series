90372eb757d284252a322e942d2a51a7224c3800
Frank Liu <fliu@proserveit.com>
2020-10-10 15:02:19 -0400

    Model Validation

--- WebApplication10/Controllers/ProductsController.cs
+++ WebApplication10/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -31,15 +32,34 @@ namespace WebApplication10.Controllers
 
         [HttpPost]
         public IActionResult Post(ProductDTO productDTO)
-        {
+        {           
+
             return Ok($"product id: {productDTO.Id}, name: {productDTO.Name}");
         }
     }   
     
     public class ProductDTO
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The name has to be less than 10 char long.")]
         public string Name { get; set; }
+
+        [ReleasedDateInPast]
+        public DateTime ReleasedDate { get; set; }
     }
     
+    public class ReleasedDateInPast: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var productDTO = (ProductDTO)validationContext.ObjectInstance;
+
+            if (productDTO.ReleasedDate >= DateTime.Today) return new ValidationResult("The product's release date has to be in the past.");
+
+            return ValidationResult.Success;
+        }
+    }
 }
